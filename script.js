0ca33a3ec2747426586bc363fe45aa280b2f9ca8
Papa Fall <papa.fall1@gmail.com>
2024-02-08 19:57:32 -0800

    Improved sketchpad function

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const squareBtn = document.querySelector("#squareBtn");
 
-squareBtn.addEventListener("click", function () {
+function sketchPad() {
   const gridContainer = document.getElementById("container");
+  gridContainer.textContent = "";
 
-  const numOfSquares = parseInt(prompt("How Many squares"));
+  const numOfSquares = parseInt(prompt("How Many squares?"));
 
   // Create 16x16 grid
   for (let i = 0; i < numOfSquares; i++) {
     for (let j = 0; j < numOfSquares; j++) {
       const gridItem = document.createElement("div");
       gridItem.classList.add("square");
+
+      const height = 400 / numOfSquares;
+      const width = 400 / numOfSquares;
+
+      gridItem.style.cssText = `height: ${height}px; width: ${width}px;`;
+
       gridContainer.appendChild(gridItem);
     }
   }
-});
+}
+
+squareBtn.addEventListener("click", sketchPad);
