ccf3848c495a09e544af06b1b5128e663de9e608
Papa Fall <papa.fall1@gmail.com>
2024-02-10 23:47:05 -0800

    completed drawing effect and added comments to code

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+//Getting the square request button
 const squareBtn = document.querySelector("#squareBtn");
+
+//Setting the grid default to 16x16
 let numOfSquares = 16;
 
+//Getting the main grid container
 const gridContainer = document.getElementById("container");
 
+//Defining when the mouse is clicked an held down
 let isMouseDown = false;
 gridContainer.addEventListener("mousedown", function () {
   isMouseDown = true;
@@ -11,23 +16,32 @@ gridContainer.addEventListener("mouseup", function () {
   isMouseDown = false;
 });
 
+//Drawing effect when mouse is clicked and held down
+//the .black class has a background color of black
+//This toggles the .black class off and on depending whether whether mouse is clicked and held down
 function draw(e) {
   if (isMouseDown) {
-    e.style.backgroundColor = "black";
+    if (!e.target.classList.contains("black")) {
+      e.target.classList.toggle("black");
+    }
   }
 }
 
+//Function to create the grid once the user enters a number
 function sketchPad() {
+  //Setting the grid to empty as a default
   gridContainer.textContent = "";
 
+  //Prompting the user to enter a number of squares
   numOfSquares = parseInt(prompt("How Many squares?"));
 
-  // Create 16x16 grid
+  // Create grid size using the number from prompt above
   for (let i = 0; i < numOfSquares; i++) {
     for (let j = 0; j < numOfSquares; j++) {
       const gridItem = document.createElement("div");
       gridItem.classList.add("square");
 
+      //Setting the grid div size by deviding the total size of container by the number of squares
       const height = 400 / numOfSquares;
       const width = 400 / numOfSquares;
 
@@ -35,6 +49,7 @@ function sketchPad() {
 
       gridContainer.appendChild(gridItem);
 
+      //Calling the drawing effecrt when mouse is clicked, held down, and dragged.
       gridItem.addEventListener("mouseover", draw);
       gridItem.addEventListener("mousedown", draw);
     }
