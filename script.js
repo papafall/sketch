0ebcb8e4bb85f2036efe2f716aef7df454c948c7
Papa Fall <papa.fall1@gmail.com>
2024-02-13 10:02:15 -0800

    Improse erase action anfd polished code

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-//Getting the buttons DOM
+// Getting the buttons DOM
 const squareBtn = document.querySelector("#squareBtn");
 const drawBtn = document.querySelector("#draw");
 const eraseBtn = document.querySelector("#erase");
 const clearBtn = document.querySelector("#clear");
-//Setting the grid default to 16x16
+
+// Setting the grid default to 16x16
 let numOfSquares = 16;
-//Getting the main grid container
+
+// Getting the main grid container
 const gridContainer = document.getElementById("container");
-//Defining when the mouse is clicked an held down
+
+// Defining when the mouse is clicked and held down
 let isMouseDown = false;
 gridContainer.addEventListener("mousedown", function () {
   isMouseDown = true;
@@ -16,9 +19,9 @@ gridContainer.addEventListener("mouseup", function () {
   isMouseDown = false;
 });
 
-//Drawing effect when mouse is clicked and held down
-//the .black class has a background color of black
-//This toggles the .black class off and on depending whether whether mouse is clicked and held down
+// Drawing effect when mouse is clicked and held down
+// The .black class has a background color of black.
+// This toggles the .black class off and on depending on whether the mouse is clicked and held down.
 function draw(e) {
   if (isMouseDown) {
     if (!e.target.classList.contains("black")) {
@@ -27,15 +30,16 @@ function draw(e) {
   }
 }
 
+// Erase function when mouse is clicked and held down
 function erase(e) {
   if (isMouseDown) {
-    if (!e.target.classList.contains("erase")) {
-      e.target.classList.toggle("erase");
+    if (e.target.classList.contains("black")) {
+      e.target.classList.remove("black");
     }
   }
 }
 
-//Function to prompt the user to enter a number and only return if it's a number between 1 and 100
+// Function to prompt the user to enter a number and only return if it's a number between 1 and 100
 function getUserNum() {
   numOfSquares = parseInt(
     prompt("Select grid size - Enter a number between 1 and 100")
@@ -49,18 +53,19 @@ function getUserNum() {
   }
 }
 
-//Function to create the grid once the user enters a number
+// Function to create the grid once the user enters a number
 function sketchPad() {
-  //Setting the grid to empty as a default
+  // Setting the grid to empty as a default
   gridContainer.textContent = "";
   getUserNum();
+
   // Create grid size using the number from prompt above
   for (let i = 0; i < numOfSquares; i++) {
     for (let j = 0; j < numOfSquares; j++) {
       const gridItem = document.createElement("div");
       gridItem.classList.add("square");
 
-      //Setting the grid div size by deviding the total size of container by the number of squares
+      // Setting the grid div size by dividing the total size of container by the number of squares
       const height = 400 / numOfSquares;
       const width = 400 / numOfSquares;
 
@@ -68,6 +73,7 @@ function sketchPad() {
 
       gridContainer.appendChild(gridItem);
 
+      // Functions to handle drawing and erasing actions
       function drawAction() {
         gridItem.addEventListener("mouseover", draw);
         gridItem.addEventListener("mousedown", draw);
@@ -80,20 +86,26 @@ function sketchPad() {
         gridContainer.addEventListener("mousemove", erase);
       }
 
+      // Initial setup for drawing
       drawAction();
 
+      // Event listeners for clear, erase, and draw buttons
       clearBtn.addEventListener("click", function () {
         gridItem.style.backgroundColor = ""; // Clear the background color
         gridItem.classList.remove("black", "erase"); // Remove both "black" and "erase" classes
       });
 
-      eraseBtn.addEventListener("click", eraseAction);
+      eraseBtn.addEventListener("click", function () {
+        eraseAction();
+        drawAction();
+      });
 
       drawBtn.addEventListener("click", function () {
         removeEraseEvents();
         drawAction();
       });
 
+      // Function to remove erase events when switching to draw mode
       function removeEraseEvents() {
         gridItem.removeEventListener("mouseover", erase);
         gridItem.removeEventListener("mousedown", erase);
@@ -103,4 +115,5 @@ function sketchPad() {
   }
 }
 
+// Event listener for the square button to initiate the sketchpad
 squareBtn.addEventListener("click", sketchPad);
